e64761b25f57fa68e7ca1682ec58e4f09cd97d24
Omar Rehan <omarrehan724@gmail.com>
2025-04-20 18:56:33 +0200

    Remove canvas

--- instructor/app/view/header.php
+++ instructor/app/view/header.php
@@ -19,7 +19,7 @@ if (!defined('NotDirectAccess')){
 	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/css/tempusdominus-bootstrap-4.min.css" />
 		<link rel="stylesheet" type="text/css" href="style/css/summernote-lite.min.css">
 		<link rel="stylesheet" type="text/css" href="style/css/instructor.css">
-		<link rel="stylesheet" type="text/css" href="style/css/percent.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/@popperjs/core@2"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
