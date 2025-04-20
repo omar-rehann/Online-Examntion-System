46f3abdd0c873e7cc4c67d596e97381536bb1f9b
Omar Rehan <omarrehan724@gmail.com>
2025-04-20 16:58:12 +0200

    Delete shufle

--- instructor/style/js/instructor.js
+++ instructor/style/js/instructor.js
@@ -513,17 +513,7 @@ function createPie(id) {
     }
 }
 
-// خلط عناصر المصفوفة عشوائياً
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
-    return a;
-}
+
 
 // إنشاء المخططات الدائرية المطلوبة
 function createPieCharts() {
