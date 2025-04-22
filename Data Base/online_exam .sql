2fabf22bbf5989266b2f6719a86a93e10fac7c53
Omar Rehan <omarrehan724@gmail.com>
2025-04-22 14:56:06 +0200

    last version

--- Data Base/online_exam .sql
+++ Data Base/online_exam .sql
@@ -3,7 +3,6 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2025 at 05:47 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -234,24 +233,6 @@ CREATE TABLE `answers` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `answers`
---
-
-INSERT INTO `answers` (`id`, `exam_id`, `question_id`, `student_id`, `is_correct`, `created_at`) VALUES
-(1, 43, 201, 201234567, 1, '2025-04-17 01:56:49'),
-(2, 43, 202, 201234567, 0, '2025-04-17 01:56:49'),
-(3, 43, 203, 201234567, 1, '2025-04-17 01:56:49'),
-(4, 43, 204, 201234567, 0, '2025-04-17 01:56:49'),
-(5, 43, 205, 201234567, 1, '2025-04-17 01:56:49'),
-(6, 43, 206, 201234567, 0, '2025-04-17 01:56:49'),
-(7, 44, 201, 201234567, 1, '2025-04-17 15:42:09'),
-(8, 44, 202, 201234567, 0, '2025-04-17 15:42:09'),
-(9, 44, 203, 201234567, 1, '2025-04-17 15:42:09'),
-(10, 44, 204, 201234567, 0, '2025-04-17 15:42:09'),
-(11, 44, 205, 201234567, 1, '2025-04-17 15:42:09'),
-(12, 44, 206, 201234567, 0, '2025-04-17 15:42:09');
-
 -- --------------------------------------------------------
 
 --
@@ -264,14 +245,6 @@ CREATE TABLE `course` (
   `parent` int(11) DEFAULT NULL,
   `instructorID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `course`
---
-
-INSERT INTO `course` (`id`, `name`, `parent`, `instructorID`) VALUES
-(70, 'c++', NULL, 31),
-(71, 'quiz', 70, 31);
 
 -- --------------------------------------------------------
 
@@ -287,13 +260,6 @@ CREATE TABLE `groups` (
   `instructorID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
 
---
--- Dumping data for table `groups`
---
-
-INSERT INTO `groups` (`id`, `name`, `assignedTest`, `settingID`, `instructorID`) VALUES
-(13, 'C', 44, 84, 31);
-
 -- --------------------------------------------------------
 
 --
@@ -305,13 +271,6 @@ CREATE TABLE `groups_has_students` (
   `studentID` int(11) NOT NULL,
   `joinDate` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `groups_has_students`
---
-
-INSERT INTO `groups_has_students` (`groupID`, `studentID`, `joinDate`) VALUES
-(13, 201234567, '2025-04-13 23:09:50');
 
 -- --------------------------------------------------------
 
@@ -336,8 +295,7 @@ CREATE TABLE `instructor` (
 --
 
 INSERT INTO `instructor` (`id`, `name`, `email`, `password`, `phone`, `password_token`, `token_expire`, `suspended`, `isAdmin`) VALUES
-(7, 'System Administrator', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '', NULL, NULL, 0, 1),
-(31, 'jamal jado', 'jamal@gmail.com', '7592628bce37dd14e0b36ea66d5ba847', '01276612119', NULL, NULL, 0, 0);
+(7, 'System Administrator', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '', NULL, NULL, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -350,18 +308,9 @@ CREATE TABLE `mails` (
   `resultID` int(11) DEFAULT NULL,
   `studentID` int(11) DEFAULT NULL,
   `instructorID` int(11) DEFAULT NULL,
-  `sends_at` timestamp NULL DEFAULT NULL,
-  `sent` tinyint(1) DEFAULT 0,
-  `type` tinyint(1) DEFAULT NULL
+  `type` tinyint(1) DEFAULT NULL,
+  `sends_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `mails`
---
-
-INSERT INTO `mails` (`id`, `resultID`, `studentID`, `instructorID`, `sends_at`, `sent`, `type`) VALUES
-(18, 43, 201234567, 31, '2025-04-17 15:42:09', 0, 2),
-(19, 43, 201234567, 31, '2025-04-17 15:42:09', 0, 3);
 
 -- --------------------------------------------------------
 
@@ -381,31 +330,6 @@ CREATE TABLE `question` (
   `deleted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
 
---
--- Dumping data for table `question`
---
-
-INSERT INTO `question` (`id`, `question`, `type`, `points`, `difficulty`, `isTrue`, `instructorID`, `courseID`, `deleted`) VALUES
-(188, 'Complete', 2, 3, 2, 0, 31, 71, 1),
-(189, 'True / False', 1, 3, 2, 0, 31, 71, 1),
-(190, 'Match The Following', 4, 2, 3, 0, 31, 71, 1),
-(191, 'What is the….?', 5, 1, 2, 0, 31, 71, 1),
-(192, 'pointer refer to value ?', 1, 5, 1, 0, 31, 71, 1),
-(193, 'define omar rehan', 1, 5, 1, 0, 31, 71, 1),
-(194, 'Complete', 2, 3, 2, 0, 31, 71, 1),
-(195, 'True / False', 1, 3, 2, 0, 31, 71, 1),
-(196, 'Match The Following', 4, 2, 3, 0, 31, 71, 1),
-(197, 'What is the….?', 5, 1, 2, 0, 31, 71, 1),
-(198, 'pointer refer to value ?', 1, 5, 1, 0, 31, 71, 1),
-(199, 'define omar rehan', 1, 5, 1, 0, 31, 71, 1),
-(200, '<p>&nbsp; &nbsp; Define Data base ?<br></p>', 5, 5, 1, 1, 31, 71, 1),
-(201, '<p>html refer to ?&nbsp;</p>', 0, 5, 1, 1, 31, 71, 0),
-(202, '<p>pointer refer to value ?&nbsp;</p>', 1, 5, 1, 1, 31, 71, 0),
-(203, '<p>&nbsp; &nbsp; what is the oop ?<br></p>', 4, 1, 1, 1, 31, 71, 0),
-(204, '<p>oop ?</p>', 2, 5, 1, 1, 31, 71, 0),
-(205, '<p>&nbsp; &nbsp; oop ?&nbsp;<br></p>', 3, 5, 1, 1, 31, 71, 0),
-(206, '<p>define data base ?</p>', 5, 5, 1, 1, 31, 71, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -420,38 +344,6 @@ CREATE TABLE `question_answers` (
   `isCorrect` tinyint(1) DEFAULT 1,
   `points` int(2) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `question_answers`
---
-
-INSERT INTO `question_answers` (`id`, `questionID`, `answer`, `matchAnswer`, `isCorrect`, `points`) VALUES
-(917, 188, 'Accepted Answer', NULL, 1, 1),
-(918, 188, 'Accepted Answer', NULL, 1, 1),
-(919, 188, 'Accepted Answer', NULL, 1, 1),
-(920, 190, 'Answer 1', 'Match 1', 1, 1),
-(921, 190, 'Answer 2', 'Match 2', 1, 1),
-(922, 190, 'Answer 3', 'Match 3', 1, 1),
-(923, 194, 'Accepted Answer', NULL, 1, 1),
-(924, 194, 'Accepted Answer', NULL, 1, 1),
-(925, 194, 'Accepted Answer', NULL, 1, 1),
-(926, 196, 'Answer 1', 'Match 1', 1, 1),
-(927, 196, 'Answer 2', 'Match 2', 1, 1),
-(928, 196, 'Answer 3', 'Match 3', 1, 1),
-(929, 201, '<p>hyper text refrnace&nbsp;</p>', NULL, 1, 1),
-(930, 201, '<p>ooc</p>', NULL, 0, 1),
-(931, 201, '<p>ood</p>', NULL, 0, 1),
-(932, 201, '<p>ccf</p>', NULL, 0, 1),
-(933, 203, 'a', '1', 1, 1),
-(934, 203, 'b', '2', 1, 1),
-(935, 203, 'c', '3', 1, 1),
-(936, 203, 'd', '4', 1, 1),
-(937, 204, 'aab', NULL, 1, 1),
-(938, 204, 'ggg', NULL, 1, 1),
-(939, 205, '<p>object orinted programming&nbsp;</p>', NULL, 1, 1),
-(940, 205, '<p>object orinted programming&nbsp;</p>', NULL, 1, 1),
-(941, 205, '<p>object orinted value ?</p>', NULL, 0, 1),
-(942, 205, '<p>object orinted ggg</p>', NULL, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -473,13 +365,6 @@ CREATE TABLE `result` (
   `score` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
 
---
--- Dumping data for table `result`
---
-
-INSERT INTO `result` (`id`, `studentID`, `testID`, `groupID`, `settingID`, `startTime`, `endTime`, `isTemp`, `hostname`, `ipaddr`, `score`) VALUES
-(43, 201234567, 44, 13, 84, '2025-04-17 15:41:36', '2025-04-17 15:42:09', 0, 'DESKTOP-P8VG2E9', '::1', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -496,21 +381,6 @@ CREATE TABLE `result_answers` (
   `points` int(3) DEFAULT -1,
   `isCorrect` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `result_answers`
---
-
-INSERT INTO `result_answers` (`id`, `resultID`, `questionID`, `answerID`, `isTrue`, `textAnswer`, `points`, `isCorrect`) VALUES
-(478, 43, 201, 929, 1, NULL, 5, 1),
-(479, 43, 202, NULL, 0, NULL, 0, 0),
-(480, 43, 203, 933, 0, '3', 0, 0),
-(481, 43, 203, 934, 0, '2', 1, 1),
-(482, 43, 203, 935, 0, '2', 0, 0),
-(483, 43, 203, 936, 0, '4', 1, 1),
-(484, 43, 204, NULL, 0, 'object orinted progarmming', 0, 0),
-(485, 43, 205, 939, 1, NULL, 0, 0),
-(486, 43, 206, NULL, 0, 'data base store large data ', 5, 1);
 
 --
 -- Triggers `result_answers`
@@ -565,13 +435,6 @@ CREATE TABLE `student` (
   `sessionID` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
 
---
--- Dumping data for table `student`
---
-
-INSERT INTO `student` (`id`, `name`, `email`, `phone`, `password`, `password_token`, `token_expire`, `suspended`, `sessionID`) VALUES
-(201234567, 'omar rehan', 'omar@gmail.com', '01276612119', '276506d3704c67d67ff9a500be50dd95', NULL, NULL, 0, '2k0n160akrsth3kfppfsqdj7i0');
-
 -- --------------------------------------------------------
 
 --
@@ -597,13 +460,6 @@ CREATE TABLE `test` (
   `instructorID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
 
---
--- Dumping data for table `test`
---
-
-INSERT INTO `test` (`id`, `name`, `courseID`, `deleted`, `instructorID`) VALUES
-(44, 'last', 70, 0, 31);
-
 -- --------------------------------------------------------
 
 --
@@ -614,18 +470,6 @@ CREATE TABLE `tests_has_questions` (
   `testID` int(11) DEFAULT NULL,
   `questionID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `tests_has_questions`
---
-
-INSERT INTO `tests_has_questions` (`testID`, `questionID`) VALUES
-(44, 201),
-(44, 202),
-(44, 203),
-(44, 204),
-(44, 205),
-(44, 206);
 
 -- --------------------------------------------------------
 
@@ -647,14 +491,6 @@ CREATE TABLE `test_settings` (
   `instructorID` int(11) DEFAULT NULL,
   `testName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
-
---
--- Dumping data for table `test_settings`
---
-
-INSERT INTO `test_settings` (`id`, `startTime`, `endTime`, `duration`, `prevQuestion`, `viewAnswers`, `releaseResult`, `sendToStudent`, `sendToInstructor`, `passPercent`, `instructorID`, `testName`) VALUES
-(83, '2025-04-17 03:53:00', '2025-04-17 05:53:00', 30, 0, 0, 1, 1, 1, 60, 31, ''),
-(84, '2025-04-17 17:41:00', '2025-04-17 18:41:00', 30, 1, 0, 1, 1, 1, 60, 31, '');
 
 --
 -- Indexes for dumped tables
@@ -784,7 +620,7 @@ ALTER TABLE `test_settings`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 
 --
 -- AUTO_INCREMENT for table `course`
@@ -808,43 +644,43 @@ ALTER TABLE `instructor`
 -- AUTO_INCREMENT for table `mails`
 --
 ALTER TABLE `mails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
 
 --
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=207;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=225;
 
 --
 -- AUTO_INCREMENT for table `question_answers`
 --
 ALTER TABLE `question_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=943;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=981;
 
 --
 -- AUTO_INCREMENT for table `result`
 --
 ALTER TABLE `result`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `result_answers`
 --
 ALTER TABLE `result_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=487;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=659;
 
 --
 -- AUTO_INCREMENT for table `test`
 --
 ALTER TABLE `test`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
 
 --
 -- AUTO_INCREMENT for table `test_settings`
 --
 ALTER TABLE `test_settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
 
 --
 -- Constraints for dumped tables
